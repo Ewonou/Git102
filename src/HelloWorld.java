306fecbbf9e640ba62bf09ca5be4f7d113d2288e
Ewonou Aziakonou <aewonou@gmail.com>
2012-07-14 18:05:23 -0500

    all Changes

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Merveille & Rejoice
+ * Date: 7/14/12
+ * Time: 4:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+}
---
+++ tests/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+* Created with IntelliJ IDEA.
+* User: Merveille & Rejoice
+* Date: 7/14/12
+* Time: 5:35 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class HelloWorldTest {
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
